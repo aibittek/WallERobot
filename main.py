34a08597d773b5a479614fe82ac4683f8a779c54
aibit <55239610@qq.com>
2021-06-13 21:54:26 +0800

    modify platform import

--- main.py
+++ main.py
@@ -15,16 +15,13 @@ sys.path.append("core/ai/ASR")
 sys.path.append("core/ai/TTS")
 sys.path.append("core/common")
 sys.path.append("core/wakeup")
-
 if platform.system() == "Linux":
     if platform.processor() == 'x86_64':
-        sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "/lib/modules/x86_64"))
+        sys.path.append('libs/modules/x86_64')
     else:
-        sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "/lib/modules/pi"))
+        sys.path.append('libs/modules/pi')
 else:
     raise ImportError("now snowboy only runs on pi or Ubuntu 16.04.")
-
-sys.path.append("libs/modules/x86_64")
 import reflect
 import play 
 from ringbuffer import RingBuffer
